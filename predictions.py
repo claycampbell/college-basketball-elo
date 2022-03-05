b022050200d1a1424c2fd2f6a70f9024f65e48a5
clay <claycampbell@gmail.com>
2022-03-05 12:45:42 -0800

    Update predictions.py
    new columns

--- predictions.py
+++ predictions.py
@@ -105,7 +105,7 @@ def predict_next_day(elo_state, forecast_date, auto):
 		else:
 			predictions.append([game[0], game[1], "{0:.0%}".format(1 - (float(prob[:-1])/100)), -home_spread, game[3], prob, home_spread])
 	predictions, timestamp = spread_enricher.add_spreads_to_todays_preds(predictions, forecast_date)
-	output = pd.DataFrame(predictions, columns = ['Neutral', 'Away', 'Away Win Prob.', 'Away Pred. Spread', 'Live Away Spread', 'Home', 'Home Win Prob.', 'Home Pred. Spread'])
+	output = pd.DataFrame(predictions, columns = ['Neutral'	,'Away','Away Win Prob.','Away Pred. Spread','Away Delta','Live Away Spread,'Home','Home Win Prob.''Home Pred. Spread',	'Home Delta','Live Home Spread'])
 	spreads_string = ''
 	if timestamp != 'N/A':
 		spreads_string = ' with Spreads as of '
